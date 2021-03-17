11d43b83352945523b6910cf0df4a02a92f328f5
athira-gopi <athugopi1999@gmail.com>
2021-03-17 15:44:59 +0530

    message

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends("theme")
+
+@section("content")
+
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-3 col-lg-3 col-xl-3 col-xxl-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit accusamus ex officiis voluptatibus earum recusandae fuga iste facilis quo eligendi! Iure voluptates error reprehenderit harum esse. Neque corrupti cum ea!</div>
+<div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat eligendi voluptates tempore exercitationem optio esse quam aut excepturi ut itaque nostrum minima iure nulla provident, accusamus id, error iusto eaque.
+</div>
+<div class="col col-12 col-sm-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+</div>
+</div>
+
+@endsection
--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+@extends("theme")
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Add</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/search">Search</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
+
+@section("content")
 
 
     <div class="container">
@@ -75,5 +55,6 @@
     <div class="col col-12 col-sm-3 col-lg-3 col-xl-3 col-xxl-3"></div>
     </div>
     </div>
-</body>
-</html>
+
+    @endsection
+    
